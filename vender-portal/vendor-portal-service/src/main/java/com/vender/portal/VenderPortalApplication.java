0ac567619c92d31b893d163995d72d16c0d176e0
Chandra <chandrais010@gmail.com>
2017-10-24 20:35:33 +0530

    first commit

---
+++ vender-portal/vendor-portal-service/src/main/java/com/vender/portal/VenderPortalApplication.java
@@ -0,0 +1,12 @@
+package com.vender.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenderPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenderPortalApplication.class, args);
+	}
+}
---
+++ vender-portal/vendor-portal-service/src/main/java/com/vender/portal/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.vender.portal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@Api(value="Test", description="Testing Swagger doc")
+public class TestController {
+
+	@ApiOperation(value = "View a list of available products", response = Iterable.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
